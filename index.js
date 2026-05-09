7c7cc03dc112a8f1c1a2f3f0f9ec3bf2e4e70dc0
Ashabdiu <114132979+Ashabdiu@users.noreply.github.com>
2026-05-09 12:24:26 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+let randomimg1 = "images/dice" + randomNumber1 + ".png";
+let randomimg2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelector(".img1").setAttribute("src", randomimg1);
+document.querySelector(".img2").setAttribute("src", randomimg2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 wins";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 wins";
+} else {
+  document.querySelector("h1").innerHTML = "Draw";
+}
